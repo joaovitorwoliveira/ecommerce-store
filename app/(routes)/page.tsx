94b9ec216dd2e5e3f4a35d7ccc5a84b4080d4693
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-05-24 17:21:33 -0300

    01- Environment setup & featured products

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,24 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("e9f0e622-f24e-43fc-826c-9b4f63148d26");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Produtos em Destaque" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+export default HomePage;
---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-sm text-black">
+          © 2024 Loja E-commerce. Todos os direitos reservados.
+        </p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
